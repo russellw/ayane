79c2e1c438a05f688bfbb2fd42d281e77abe887d
Russell Wallace <russell.wallace@gmail.com>
2021-01-20 05:37:39 +0000

    build MPIR

---
+++ build_mpir.bat
@@ -0,0 +1,8 @@
+rem MPIR is the Windows-friendly port of GMP for arbitrary precision arithmetic
+rem At the time of writing, the latest version can be downloaded from:
+rem https://github.com/BrianGladman/mpir
+rem The following parameters can be adjusted for architecture and compiler version
+
+cd \mpir\msvc\vs19
+call msbuild.bat gc DLL x64 Debug
+call msbuild.bat gc LIB x64 Release

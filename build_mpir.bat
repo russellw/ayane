561a6560f8860252d5627d4d20a8505c67a748a5
Russell Wallace <russell.wallace@gmail.com>
2021-01-20 20:52:26 +0000

    mpir build dll

--- build_mpir.bat
+++ build_mpir.bat
@@ -3,6 +3,8 @@ rem At the time of writing, the latest version can be downloaded from:
 rem https://github.com/BrianGladman/mpir
 rem The following parameters can be adjusted for architecture and compiler version
 
-cd \mpir\msvc\vs19
+pushd \mpir\msvc\vs19
 call msbuild.bat gc DLL x64 Debug
 call msbuild.bat gc LIB x64 Release
+popd
+copy \mpir\dll\x64\Debug\mpir.dll

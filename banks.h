c4668ca0b5b048db46069da8d1144d350100abeb
Russell Wallace <russell.wallace@gmail.com>
2021-01-21 15:13:56 +0000

    refactor

--- banks.h
+++ banks.h
@@ -1,52 +1,52 @@
 template <class T> class bank {
   int cap = 0x100;
   int count = 0;
-  T **p = (T **)xcalloc(cap, sizeof(T *));
+  T **entries = (T **)xcalloc(cap, sizeof(T *));
 
-  bool eq(T *a, const void *s, int bytes) {
-    if (a->n != bytes)
+  bool eq(T *a, const char *s, int n) {
+    if (a->n != n)
       return false;
-    return !memcmp(a->s, s, bytes);
+    return !memcmp(a->s, s, n);
   }
 
-  int slot(T **p, int cap, const void *s, int bytes) {
+  int slot(T **entries, int cap, const char *s, int n) {
     auto mask = cap - 1;
-    auto i = fnv(s, bytes) & mask;
-    while (p[i] && !eq(p[i], s, bytes))
+    auto i = fnv(s, n) & mask;
+    while (entries[i] && !eq(entries[i], s, n))
       i = (i + 1) & mask;
     return i;
   }
 
   void expand() {
     auto cap1 = cap * 2;
-    auto p1 = (T **)xcalloc(cap1, sizeof(T *));
+    auto entries1 = (T **)xcalloc(cap1, sizeof(T *));
     for (int i = 0; i != cap; ++i) {
-      auto a = p[i];
+      auto a = entries[i];
       if (a)
-        p1[slot(p1, cap1, a->s, a->n)] = a;
+        entries1[slot(entries1, cap1, a->s, a->n)] = a;
     }
-    free(p);
+    free(entries);
     cap = cap1;
-    p = p1;
+    entries = entries1;
   }
 
 public:
   void init(T *a) {
     ++count;
     assert(count <= cap * 3 / 4);
-    auto i = slot(p, cap, a->s, a->n);
-    assert(!p[i]);
-    p[i] = a;
+    auto i = slot(entries, cap, a->s, a->n);
+    assert(!entries[i]);
+    entries[i] = a;
   }
 
-  T *intern(const void *s, int bytes) {
-    auto i = slot(p, cap, s, bytes);
-    if (p[i])
-      return p[i];
+  T *intern(const char *s, int n) {
+    auto i = slot(entries, cap, s, n);
+    if (entries[i])
+      return entries[i];
     if (++count > cap * 3 / 4) {
       expand();
-      i = slot(p, cap, s, bytes);
+      i = slot(entries, cap, s, n);
     }
-    return p[i] = store(s, bytes);
+    return entries[i] = store(s, n);
   }
 };

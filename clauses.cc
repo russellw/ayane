73294a346af65f051ebb16ad39cd12b0148e3483
Russell Wallace <russell.wallace@gmail.com>
2021-02-14 03:31:49 +0000

    clause

--- clauses.cc
+++ clauses.cc
@@ -93,58 +93,66 @@ clause *mkclause(w infer, clause *from, clause *from1) {
   sort(neg.begin(), neg.end());
   sort(pos.begin(), pos.end());
 
-  // remove duplicate literals
+  // remove duplicate literals (must be done after sorting, because in order to
+  // run in linear time, std::unique assumes duplicates will be consecutive)
   neg.erase(unique(neg.begin(), neg.end()), neg.end());
   pos.erase(unique(pos.begin(), pos.end()), pos.end());
 
-  // check for tautology
-  for (auto a : neg)
-    if (a == basic(b_false))
-      return 0;
-  for (auto a : pos)
-    if (a == basic(b_true))
-      return 0;
-  for (auto a : neg)
-    for (auto b : pos)
-      if (a == b)
-        return 0;
-
   // gather literals
+  auto p = neg.p;
   auto nn = neg.n;
-  auto pn = pos.n;
-  auto n = nn + pn;
+  auto np = pos.n;
+  auto n = nn + np;
+  // postcondition: the negative and positive input arrays are cleared. this
+  // means callers can assume they are clear to start with, and avoids
+  // duplicating this line of code at many call sites
   neg.n = pos.n = 0;
-  if (n > min(sizeof neg.p / sizeof *neg.p, (size_t)0xffff)) {
+  if (n > min(sizeof neg.p / sizeof *p, (size_t)0xffff)) {
     // if the number of literals would exceed 16-bit count, discard the clause.
     // in principle this breaks completeness, though in practice it is unlikely
     // such a clause would contribute anything useful to the proof search anyway
     complete = 0;
     return 0;
   }
-  memcpy(neg.p + nn, pos.p, pn * sizeof *pos.p);
+  memcpy(p + nn, pos.p, np * sizeof *p);
+
+  // check for tautology (could be done earlier, but as the only step that takes
+  // quadratic time, it is here being done after the efforts to reduce the
+  // number of literals)
+  for (auto i = p, e = p + nn; i != e; ++i)
+    if (*i == basic(b_false))
+      return 0;
+  for (auto i = p + nn, e = p + n; i != e; ++i)
+    if (*i == basic(b_true))
+      return 0;
+  for (auto i = p, e = p + nn; i != e; ++i) {
+    auto a = *i;
+    for (auto j = p + nn, ej = p + n; j != ej; ++j)
+      if (a == *j)
+        return 0;
+  }
 
   // check for an identical existing clause. unlike some other datatypes, we
   // return null rather than just returning a pointer to the existing object,
   // because creating a new clause is a significant action; caller may need to
   // know whether to go ahead and add the new clause to various containers
-  auto i = slot(entries, cap, neg.p, nn, n);
+  auto i = slot(entries, cap, p, nn, n);
   if (entries[i])
     return 0;
   if (++count > cap * 3 / 4) {
     expand();
-    i = slot(entries, cap, neg.p, nn, n);
+    i = slot(entries, cap, p, nn, n);
   }
 
   // make new clause
-  auto c = entries[i] =
-      (clause *)xmalloc(offsetof(clause, v) + n * sizeof *neg.p);
+  auto c = entries[i] = (clause *)xmalloc(offsetof(clause, v) + n * sizeof *p);
   memset(c, 0, offsetof(clause, v));
   c->infer = infer;
   c->nn = nn;
   c->n = n;
   c->from[0] = from;
   c->from[1] = from1;
-  memcpy(c->v, neg.p, n * sizeof *neg.p);
+  memcpy(c->v, p, n * sizeof *p);
   return c;
 }
 ///

9f399cad56a73effdcfbac352faa1b75894d3b50
Russell Wallace <russell.wallace@gmail.com>
2021-03-02 22:02:01 +0000

    check

--- check.bat
+++ check.bat
@@ -1,3 +1,3 @@
 rem Use clang for error/warning check
-clang -DDEBUG -I\mpir -Wall -Wno-char-subscripts -Wno-deprecated-declarations -Wno-string-plus-char -c *.cc
+clang -DDEBUG -I\mpir -Wall -Wno-char-subscripts -Wno-deprecated-declarations -Wno-string-plus-char -Wno-unused-function -c *.cc
 del *.o

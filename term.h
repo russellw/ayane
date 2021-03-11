156de82a4cd37751af541d9bef3d5f7c820f7ac6
Russell Wallace <russell.wallace@gmail.com>
2021-03-11 04:22:54 +0000

    sort

--- term.h
+++ term.h
@@ -63,28 +63,6 @@ inline term tag(term a) { return term((uint64_t)a & (1 << tagbits) - 1); }
 
 inline uint64_t rest(term a) { return (uint64_t)a >> tagbits; }
 
-// variables
-inline term var(type t, si i) {
-  assert(!iscompound(t));
-  // variable is composed of:
-  // 6 bits tag
-  // 16 bits type
-  // 1 bit flag e.g. for renamed variables
-  // and the rest of the bits are for a number that identifies the variable
-  return term((uint64_t)i << (1 + 16 + tagbits) | (uint64_t)t << tagbits |
-              (uint64_t)term::Var);
-}
-
-inline si vari(term a) {
-  assert(tag(a) == term::Var);
-  return (uint64_t)a >> (1 + 16 + tagbits);
-}
-
-inline type vartype(term a) {
-  assert(tag(a) == term::Var);
-  return type((uint64_t)a >> tagbits);
-}
-
 // requires atomic term tag numbers to be contiguous
 inline bool iscompound(term a) { return tag(a) > term::Var; }
 
@@ -112,22 +90,46 @@ inline term at(term a, si i) {
   return begin(a)[i];
 }
 
-// SORT
-extern ary<term> freevars;
-extern si skolemi;
-///
-
-// SORT
-void getfree(term a);
-term imp(term a, term b);
-void init_terms();
 term mk(term op, const ary<term> &args);
 term mk(term op, const vec<term> &args);
 term mk(term op, term a);
 term mk(term op, term a, term b);
 term mk(term op, term a, term b, term c);
-///
 
+term imp(term a, term b);
+
+// variables
+inline term var(type t, si i) {
+  assert(!iscompound(t));
+  // variable is composed of:
+  // 6 bits tag
+  // 16 bits type
+  // 1 bit flag e.g. for renamed variables
+  // and the rest of the bits are for a number that identifies the variable
+  return term((uint64_t)i << (1 + 16 + tagbits) | (uint64_t)t << tagbits |
+              (uint64_t)term::Var);
+}
+
+inline si vari(term a) {
+  assert(tag(a) == term::Var);
+  return (uint64_t)a >> (1 + 16 + tagbits);
+}
+
+inline type vartype(term a) {
+  assert(tag(a) == term::Var);
+  return type((uint64_t)a >> tagbits);
+}
+
+extern ary<term> freevars;
+
+void getfree(term a);
+
+// etc
+extern si skolemi;
+
+void init_terms();
+
+// check terms for structural consistency
 #ifdef DEBUG
 void ck(term a);
 #else
